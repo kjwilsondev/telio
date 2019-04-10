ab56a025675a5efd131141197c9e46f2cbf832aa
KJ Wilson <yourboykjdev@gmail.com>
2019-04-09 21:46:17 -0700

    palindromes pass all tests :dancer:

--- challenges/palindromes.py
+++ challenges/palindromes.py
@@ -18,17 +18,18 @@ def is_palindrome(text):
 
 
 def is_palindrome_iterative(text):
-    # fix text to pass test
-    text = text.lower()
-    text = text.translate(str.maketrans('','', string.punctuation))
-    print(text)
     # Set indexes at opposite ends
     left = 0
     right = len(text) - 1
     # Loops until indexes meet
     while left <= right:
+        # Ignore space and punctuation
+        while text[left] == " " or text[left] in string.punctuation:
+            left += 1
+        while text[right] == " " or text[right] in string.punctuation:
+            right -= 1
         # Check for match
-        if text[left] != text[right]:
+        if text[left].lower() != text[right].lower():
             return False
         left += 1
         right -= 1
@@ -37,17 +38,22 @@ def is_palindrome_iterative(text):
 
 def is_palindrome_recursive(text, left=None, right=None):
     # fix text to pass test
-    text = text.lower()
-    text = text.translate(str.maketrans('','', string.punctuation))
-    print(text)
+    # text = text.lower()
+    # text = text.translate(str.maketrans('','', string.punctuation))
+
     # Initializes indexes
     if right == None:
         left = 0
         right = len(text) - 1
     # Checks if indexes have met
     if left <= right:
-        # Check if indexes are same
-        if text[left] == text[right]:
+        # Checks if index contains punctuation or space
+        if text[left] == " " or text[left] in string.punctuation:
+            return is_palindrome_recursive(text, left+1, right)
+        if text[right] == " " or text[right] in string.punctuation:
+            return is_palindrome_recursive(text, left, right-1)
+        # Check if indexes are same (ignores caps)
+        if text[left].lower() == text[right].lower():
             return is_palindrome_recursive(text, left+1, right-1)
         return False
     return True
@@ -65,6 +71,13 @@ def main():
     else:
         print('Usage: {} string1 string2 ... stringN'.format(sys.argv[0]))
         print('  checks if each argument given is a palindrome')
+
+def refine_text(text):
+    text = text.lower()
+    exclusions = "?.-! ,'"
+    for char in exclusions:
+        text = text.replace(char, '')
+    return text
 
 
 if __name__ == '__main__':
