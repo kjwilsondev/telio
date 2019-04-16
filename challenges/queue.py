5a53dd89d66db2071a4e92f57ad68933e2ec9502
KJ Wilson <yourboykjdev@gmail.com>
2019-04-16 13:13:13 -0700

    drafted linkedqueue :trollface:

--- challenges/queue.py
+++ challenges/queue.py
@@ -20,28 +20,57 @@ class LinkedQueue(object):
         return 'Queue({} items, front={})'.format(self.length(), self.front())
 
     def is_empty(self):
-        """Return True if this queue is empty, or False otherwise."""
-        # TODO: Check if empty
+        """
+        Return True if this queue is empty
+        or
+        False, if otherwise
+        """
+        # Check if empty
+        print("WE MADE IT")
+        print(self.list.size == 0)
+        return self.list.size == 0
 
     def length(self):
         """Return the number of items in this queue."""
-        # TODO: Count number of items
+        return self.list.size
+
 
     def enqueue(self, item):
-        """Insert the given item at the back of this queue.
-        Running time: O(???) – Why? [TODO]"""
-        # TODO: Insert given item
+        """
+        Inserts the given item at the back of this queue.
+
+        Running time: O(1) – Function prepend does not transverse through items
+        """
+        # Insert given item
+        return self.list.prepend(item)
+
 
     def front(self):
-        """Return the item at the front of this queue without removing it,
-        or None if this queue is empty."""
-        # TODO: Return front item, if any
+        """
+        Returns the item at the front of this queue without removing it
+        or 
+        None if this queue is empty
+        """
+        # Returns front item, if any
+        if self.is_empty():
+            return None
+        return self.list.tail.data
 
     def dequeue(self):
-        """Remove and return the item at the front of this queue,
-        or raise ValueError if this queue is empty.
-        Running time: O(???) – Why? [TODO]"""
+        """
+        Removes and returns the item at the front of this queue,
+        or 
+        raises ValueError if this queue is empty
+
+        Running time: O(1) – Returns tail data then dips
+        """
         # TODO: Remove and return front item, if any
+        if self.is_empty():
+            raise ValueError('Queue is empty')
+        data = self.list.tail.data
+        self.list.tail = self.list.tail.previous
+        self.list.size -= 1
+        return data
 
 
 # Implement ArrayQueue below, then change the assignment at the bottom
