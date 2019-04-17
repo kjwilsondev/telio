2e57df104fd162c95ff721e2693caac5090c9a37
KJ Wilson <yourboykjdev@gmail.com>
2019-04-16 19:08:10 -0700

    added more tests to strings :memo:

--- challenges/strings.py
+++ challenges/strings.py
@@ -7,37 +7,8 @@ def contains(text, pattern, letter=None, patterndex=0):
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
 
-    # Recursive Implementation
-
-    # # Initialize values
-    # if letter is None:
-    #     letter = 0
-
-    # if patterndex == len(pattern):
-    #     print(True)
-    #     return True
-    
-    # # Skips over letters 
-    # # that dont match first letter of pattern
-    # while text[letter] != pattern[patterndex]:
-    #     print(text[letter])
-    #     letter += 1
-    #     if letter >= len(text):
-    #         print(False)
-    #         return False
-    
-    # # Checks if next letter in text
-    # # matches the next letter in the pattern
-    # if text[letter] != pattern[patterndex]:
-    #     # if not sends it back to while loop with new start
-        
-    #     return contains(text, pattern, letter)
-    # return contains(text, pattern, letter+1, patterndex+1)
-
     if pattern in text:
-        print(True)
         return True
-    print(False)
     return False
 
 
@@ -49,29 +20,6 @@ def find_index(text, pattern, letter=None, patterndex=0):
     """
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
-    
-    # # Initialize values
-    # if letter is None:
-    #     letter = 0
-
-    # if patterndex == len(pattern):
-    #     print(letter-patterndex)
-    #     return (letter - patterndex)
-    
-    # # Skips over letters 
-    # # that dont match first letter of pattern
-    # while text[letter] != pattern[patterndex]:
-    #     letter += 1
-    #     if letter >= len(text):
-    #         print(None)
-    #         return None
-    
-    # # Checks if next letter in text
-    # # matches the next letter in the pattern
-    # if text[letter] != pattern[patterndex]:
-    #     # if not sends it back to while loop with new start
-    #     return find_index(text, pattern, letter)
-    # return find_index(text, pattern, letter+1, patterndex+1)
 
     indices = find_all_indexes(text, pattern)
     if len(indices) > 0:
@@ -86,18 +34,9 @@ def find_all_indexes(text, pattern):
     """
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
-    # # Initialize values and index array
-    indarray = []
-    # patterndex = 0
 
-    # empty string
-    if pattern == "":
-        print("empty pattern")
-        for index, letter in enumerate(text):
-            indarray.append(index)
-        return indarray
-    
-    # # File through each letter in text
+    # FIRST ATTEMPT
+    # File through each letter in text
     # for i, letter in enumerate(text):
     #     print(i, letter)
     #     # if letter match is found
@@ -115,14 +54,24 @@ def find_all_indexes(text, pattern):
     #         indarray.append(i - (len(pattern) - 1))
     #         patterndex = 0
 
-    # # Returns index array
-    # print(indarray)
-    # return indarray
+    # 
+    # Initialize index array
+    indices = []
+
+    # Empty string
+    if pattern == "":
+        print("Empty Pattern")
+        for index, letter in enumerate(text):
+            indices.append(index)
+        return indices
+    
+    # Class Implementation
+    # https://github.com/lvreynoso/CS1.3-Coursework/blob/master/strings.py
     indices = []
     candidates = {}
     delta = len(pattern)
     for position, character in enumerate(text):
-        # try to match all the first letters of the pattern
+        # find match all the first letters of the pattern
         if character == pattern[0]:
             candidates[position] = 1
         for index, streak in candidates.items():
@@ -136,9 +85,6 @@ def find_all_indexes(text, pattern):
                     candidates[index] = False
     print(indices)
     return indices
-
-    
-
 
 def test_string_algorithms(text, pattern):
     found = contains(text, pattern)
