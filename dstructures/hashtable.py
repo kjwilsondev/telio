73cb7051236cd665b074f0a7ff3f111d3b5c15bc
KJ Wilson <yourboykjdev@gmail.com>
2019-04-23 14:31:39 -0700

    reworked hashtable into hashset :zap:

--- dstructures/hashtable.py
+++ dstructures/hashtable.py
@@ -195,8 +195,12 @@ class HashTable(object):
         Should be called automatically when load factor exceeds a threshold
         such as 0.75 after an insertion (when set is called with a new key)
 
-        Best and worst case running time: ??? under what conditions? [TODO]
-        Best and worst case space usage: ??? what uses this memory? [TODO]"""
+        Running time: O(6n) => O(n)
+        - self.items() traverses through all of entries making it O(n)
+        - self.buckets creates an array of double the amount of linkedlists 
+          as entries making it O(2n)
+        - set funciton which hashes each item is O(3n)
+        """
         # If unspecified, choose new size dynamically based on current size
         if new_size is None:
             new_size = len(self.buckets) * 2  # Double size 
@@ -206,7 +210,7 @@ class HashTable(object):
         # Get a list to temporarily hold all current key-value entries
         kventries = self.items() # O(n)
         # Create a new list of new_size total empty linked list buckets
-        self.buckets = [LinkedList() for i in range(new_size)] # O(n)
+        self.buckets = [LinkedList() for i in range(new_size)] # O(2n)
         self.size = 0
         # Insert each key-value entry into the new list of buckets,
         # which will rehash them into a new bucket index based on the new size
--- dstructures/set.py
+++ dstructures/set.py
@@ -2,6 +2,9 @@
 
 from linkedlist import LinkedList
 
+# Reworked Hash Table to store one item
+# rather than storing tuples
+
 class HashSet(object):
 
     def __init__(self, init_size=16, elements=None):
@@ -14,7 +17,7 @@ class HashSet(object):
 
     def __str__(self):
         """Returns a formatted string representation of this hash set"""
-        items = ['{!r}: {!r}'.format(item) for item in self.items()]
+        items = ['item: {!r}'.format(item) for item in self.items()]
         return '{' + ', '.join(items) + '}'
 
     def __repr__(self):
@@ -73,78 +76,76 @@ class HashSet(object):
         entry = bucket.find(lambda item_value: item_value == item) # O(n)
         return entry is not None  # True or False
 
-    def get(self, key):
+    def get(self, item):
         """
-        Returns the value associated with the given key
+        Returns the value associated with the given item
         or 
         raise KeyError
 
         Running time: O(2n) => O(n)
         - hash funciton in bucket index is O(n) where n is the width of a number
-        - find function traverses through key values making it O(n)
+        - find function traverses through items making it O(n)
         """
-        # Find the bucket the given key belongs in
-        index = self._bucket_index(key) # O(n)
+        # Find the bucket the given item belongs in
+        index = self._bucket_index(item) # O(n)
         bucket = self.buckets[index] # O(1)
-        # Find the entry with the given key in that bucket, if one exists
-        entry = bucket.find(lambda key_value: key_value[0] == key) # O(n)
+        # Find the entry with the given item in that bucket, if one exists
+        entry = bucket.find(lambda item_value: item_value[0] == item) # O(n)
         if entry is not None:  # Found
-            # Return the given key's associated value
-            assert isinstance(entry, tuple)
-            assert len(entry) == 2
-            return entry[1]
+            # Return the given item's associated value
+            return entry
         else:  # Not found
-            raise KeyError('Key not found: {}'.format(key))
+            raise KeyError('Item not found: {}'.format(item))
 
-    def set(self, key, value):
+    def set(self, item):
         """
-        Inserts or updates the given key with its associated value
+        Inserts or updates the given item
 
         Running time: O(3n) => O(n)
         - hash funciton in bucket index is O(n) where n is the width of a number
-        - find function traverses through key values making it O(n)
+        - find function traverses through items making it O(n)
         - if load factor > .75, the resize method creates a new linkedlist making it O(n)
         """
-        # Find the bucket the given key belongs in
-        index = self._bucket_index(key) # O(n)
+        # Find the bucket the given item belongs in
+        index = self._bucket_index(item) # O(n)
         bucket = self.buckets[index] # O(1)
-        # Find the entry with the given key in that bucket, if one exists
-        # Check if an entry with the given key exists in that bucket
-        entry = bucket.find(lambda key_value: key_value[0] == key)
+        # Find the entry with the given item in that bucket, if one exists
+        # Check if an entry with the given item exists in that bucket
+        entry = bucket.find(lambda item_value: item_value[0] == item)
         if entry is not None:  # Found
-            # In this case, the given key's value is being updated
-            # Remove the old key-value entry from the bucket first
+            # In this case, the given item is being updated
+            # Remove the old item entry from the bucket first
             bucket.delete(entry) # O(n)
             self.size -= 1
-        # Insert the new key-value entry into the bucket in either case
-        bucket.append((key, value))
+        # Insert the new item entry into the bucket in either case
+        bucket.append(item)
         self.size += 1
         # Check if the load factor exceeds a threshold such as 0.75
         if self.load_factor() > 0.75:
             # If so, automatically resize to reduce the load factor
             self._resize() # O(n)
 
-    def delete(self, key):
+    def delete(self, item):
         """
-        Deletes the given key and its associated value
+        Deletes the given item
         or 
         raise KeyError
 
         Running time: O(2n) => O(n)
         - hash funciton in bucket index is O(n) where n is the width of a number
-        - find function traverses through key values making it O(n)
+        - find function traverses through items making it O(n)
         """
-        # Find the bucket the given key belongs in
-        index = self._bucket_index(key)
+        # Find the bucket the given item belongs in
+        index = self._bucket_index(item)
         bucket = self.buckets[index]
-        # Find the entry with the given key in that bucket, if one exists
-        entry = bucket.find(lambda key_value: key_value[0] == key)
+        # Find the entry with the given item in that bucket, if one exists
+        entry = bucket.find(lambda item_value: item_value[0] == item)
         if entry is not None:  # Found
-            # Remove the key-value entry from the bucket
+            # Remove the item entry from the bucket
             bucket.delete(entry) # O(n)
             self.size -= 1
         else:  # Not found
-            raise KeyError('Key not found: {}'.format(key))
+            raise KeyError('Item not found: {}'.format(item))
 
         # If the hash table is close to empty 
         # then divide the size of hash table in half by passing in 0
@@ -154,12 +155,16 @@ class HashSet(object):
 
     def _resize(self, new_size=None):
         """
-        Resize this hash table's buckets and rehash all key-value entries.
-        Should be called automatically when load factor exceeds a threshold
+        Resizes the hash set buckets and rehashes all entries.
+        Called automatically when load factor exceeds a threshold
         such as 0.75 after an insertion (when set is called with a new key)
 
-        Best and worst case running time: ??? under what conditions? [TODO]
-        Best and worst case space usage: ??? what uses this memory? [TODO]"""
+        Running time: O(6n) => O(n)
+        - self.items() traverses through all of entries making it O(n)
+        - self.buckets creates an array of double the amount of linkedlists 
+          as entries making it O(2n)
+        - set funciton which hashes each item is O(3n)
+        """
         # If unspecified, choose new size dynamically based on current size
         if new_size is None:
             new_size = len(self.buckets) * 2  # Double size 
@@ -167,61 +172,14 @@ class HashSet(object):
         elif new_size is 0:
             new_size = len(self.buckets) / 2  # Half size
         # Get a list to temporarily hold all current key-value entries
-        kventries = self.items() # O(n)
+        entries = self.items() # O(n)
         # Create a new list of new_size total empty linked list buckets
         self.buckets = [LinkedList() for i in range(new_size)] # O(n)
         self.size = 0
         # Insert each key-value entry into the new list of buckets,
         # which will rehash them into a new bucket index based on the new size
-        for key, value in kventries:
-            self.set(key, value)
+        for item in entries:
+            self.set(item)
 
 
-def test_hash_table():
-    ht = HashTable(4)
-    print('HashTable: ' + str(ht))
-
-    print('Setting entries:')
-    ht.set('I', 1)
-    print('set(I, 1): ' + str(ht))
-    ht.set('V', 5)
-    print('set(V, 5): ' + str(ht))
-    print('size: ' + str(ht.size))
-    print('length: ' + str(ht.length()))
-    print('buckets: ' + str(len(ht.buckets)))
-    print('load_factor: ' + str(ht.load_factor()))
-    ht.set('X', 10)
-    print('set(X, 10): ' + str(ht))
-    ht.set('L', 50)  # Should trigger resize
-    print('set(L, 50): ' + str(ht))
-    print('size: ' + str(ht.size))
-    print('length: ' + str(ht.length()))
-    print('buckets: ' + str(len(ht.buckets)))
-    print('load_factor: ' + str(ht.load_factor()))
-
-    print('Getting entries:')
-    print('get(I): ' + str(ht.get('I')))
-    print('get(V): ' + str(ht.get('V')))
-    print('get(X): ' + str(ht.get('X')))
-    print('get(L): ' + str(ht.get('L')))
-    print('contains(X): ' + str(ht.contains('X')))
-    print('contains(Z): ' + str(ht.contains('Z')))
-
-    print('Deleting entries:')
-    ht.delete('I')
-    print('delete(I): ' + str(ht))
-    ht.delete('V')
-    print('delete(V): ' + str(ht))
-    ht.delete('X')
-    print('delete(X): ' + str(ht))
-    ht.delete('L')
-    print('delete(L): ' + str(ht))
-    print('contains(X): ' + str(ht.contains('X')))
-    print('size: ' + str(ht.size))
-    print('length: ' + str(ht.length()))
-    print('buckets: ' + str(len(ht.buckets)))
-    print('load_factor: ' + str(ht.load_factor()))
-
-
-if __name__ == '__main__':
-    test_hash_table()
+# if __name__ == '__main__':
