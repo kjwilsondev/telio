6f73b879bbd0b59ed009cc18b3e3847ecb17cb7d
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2019-05-12 17:20:19 -0700

    💩 updated shitty solution

--- project/call_routing_v0.py
+++ project/call_routing_v0.py
@@ -4,27 +4,32 @@ def call_routing(prefix_file, number):
     """
     Both params are strings. 
     """
-    prefix_list = []
-    prefix_split = prefix_file.split('\n')
-    print("INITIAL SPLIT", prefix_split)
 
-    for line in prefix_split:
+    """Load words from dictionary"""
+    file = open(prefix_file, 'r')
+    prefixes = file.readlines()
+    # print(prefixes)
+    file.close()
+    
+    prefix_list = []
+
+    for line in prefixes:
         prefix_list.append(line.split(','))
-    print(prefix_list)
+        # print(prefix_list)
 
     for prefix in prefix_list:
-        pre_len = len(prefix[0]) - 1
-        num_prefix = "+" + number[:pre_len]
-        print("number prefix and current", num_prefix, "and",  prefix[0])
+        pre_len = len(prefix[0])
+        num_prefix = number[:pre_len]
         if num_prefix == prefix[0]:
             return number + "," + prefix[1]
         else: 
-            print
+            print("number prefix and current", num_prefix, "and",  prefix[0])
+        
 
 
 prefixes = """+1512, 0.04
 +1415, 0.02
 """
-number = "15124156620"
+number = "+1667565111"
 
-print(call_routing(prefixes, number))
+print(call_routing('route-costs-600.txt', number))
