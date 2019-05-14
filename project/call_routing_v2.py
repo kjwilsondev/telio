b3187659f7f16558af08b12bf17f05113f2805a2
KJ Wilson <yourboykjdev@gmail.com>
2019-05-14 01:25:06 -0700

    call routing v2 is ready for testing :rocket:

---
+++ project/call_routing_v2.py
@@ -0,0 +1,115 @@
+#python3
+
+
+# Helpful:
+# https://stackoverflow.com/questions/4989763/when-is-it-better-to-use-zip-instead-of-izip
+# In Python 3 the built-in zip does the same job as itertools.izip
+# 2.X returns an iterator instead of a list.
+# https://github.com/nschloe/matplotlib2tikz/issues/20
+try:
+    # Python 2
+    from itertools import izip
+except ImportError:
+    # Python 3
+    izip = zip
+
+class CallRoutes(object):
+    def __init__(self, *route_pages):
+        """
+        initialize routes
+        """
+        self.routes = {}
+        route_holder = {}
+        # input : route page text files
+        # output: dictiorary of key: prefixes, value: costs
+        for route_page in route_pages:
+            route_holder = self.split_prefixes(route_page)
+            # set update method
+            self.routes.update(route_holder) # O(len(n))
+
+    def __iter__(self):
+        """
+        Iterates through items in self.routes 
+        """
+        return self._generator()
+
+    def _generator(self):
+        """
+        Returns items in set
+        """
+        for item in self.routes.items():
+            yield item
+    
+    def print_routes(self):
+        # print(self.routes)
+        print([(key, self.routes[key]) for key in sorted(self.routes)])
+
+    def split_prefixes(self, prefixes):
+        """
+        return dictionary of routes from text file
+        """
+        file = open(prefixes, 'r')
+        prefixes_list = file.readlines()
+        file.close()
+
+        prefix_dict = {}
+        for prefix_line in prefixes_list:
+            if "," in prefix_line:
+                # Splits string into prefix and cost
+                prefix, cost = prefix_line.split(",")
+                # puts in (key: prefix, value: cost) and takes out \n
+                prefix_dict[prefix] = cost.strip('\n')
+
+        return prefix_dict
+
+    def call_cost(self, phone_number):
+        """
+        returns string of cost of phone number
+        """
+        # holds original number
+        original_number = phone_number
+        while len(phone_number) > 0:
+            # checks if phone number is in routes
+            if phone_number in self.routes:
+                cost = self.routes[phone_number]
+                # print(f"{phone_number} cost found: {cost}")
+                # adds phone number to dictionary for convience
+                self.routes[original_number] = cost
+                return cost
+            # Removes last character from string
+            phone_number = phone_number[:-1]
+            # print("finding...")
+        
+        return None
+    
+    def output_number_costs(self, *phone_number_lists):
+        """
+        input: text files of phone number lists
+        output: returns new text file of all phone numbers with costs
+        """
+        # initialize a txt file
+        txt = open("number-costs.txt","w+")
+
+        for phone_list in phone_number_lists:
+            # number_holders is array of phone numbers
+            pl = open(phone_list, 'r')
+            # splits numbers into elements in number_list
+            number_list = pl.readlines()
+            pl.close()
+
+            for number in number_list:
+                number = number.strip("\n")
+                # inputs number into call_cost function to find cost
+                cost = self.call_cost(number)
+                # writes phone number, cost onto the text file
+                txt.write(f"{number},{cost}\r\n")
+
+        txt.close()
+        return
+
+if __name__ == "__main__":
+    c = CallRoutes("route-costs-1.txt", "route-costs-2.txt")
+    c.output_number_costs("phone-numbers.txt")
+    # print(c.call_cost("+86153892109"))
+    # c.print_routes()
+    
